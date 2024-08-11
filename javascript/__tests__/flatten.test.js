49177acc7ca636eeb7ed6b8a8a925227cc76fb43
Mohammad Reza Ghasemi <mo.ghasemi@sadad.co.ir>
2024-08-11 16:14:37 +0330

    feat: implement the flatten function

---
+++ javascript/__tests__/flatten.test.js
@@ -0,0 +1,53 @@
+const flatten = require('../flatten');
+
+describe('flatten', () => {
+  test('flattens a simple nested object', () => {
+    const input = { a: { b: { c: 'd' } } };
+    const expectedOutput = { 'a.b.c': 'd' };
+    expect(flatten(input)).toEqual(expectedOutput);
+  });
+
+  test('flattens a more complex nested object', () => {
+    const input = {
+      a: {
+        b: {
+          c: 'd',
+          e: 'f',
+        },
+        g: 'h',
+      },
+      i: 'j',
+    };
+    const expectedOutput = {
+      'a.b.c': 'd',
+      'a.b.e': 'f',
+      'a.g': 'h',
+      'i': 'j',
+    };
+    expect(flatten(input)).toEqual(expectedOutput);
+  });
+
+  test('handles an object with an array', () => {
+    const input = {
+      a: {
+        b: [1, 2, 3],
+      },
+    };
+    const expectedOutput = {
+      'a.b': [1, 2, 3],
+    };
+    expect(flatten(input)).toEqual(expectedOutput);
+  });
+
+  test('handles an already flat object', () => {
+    const input = { 'a.b.c': 'd' };
+    const expectedOutput = { 'a.b.c': 'd' };
+    expect(flatten(input)).toEqual(expectedOutput);
+  });
+
+  test('handles an empty object', () => {
+    const input = {};
+    const expectedOutput = {};
+    expect(flatten(input)).toEqual(expectedOutput);
+  });
+});
--- javascript/flatten.js
+++ javascript/flatten.js
@@ -4,14 +4,29 @@
  *
  * @param {object} object An object that may be nested.
  *
+ * @param parentKey
+ * @param result
  * @returns flatten object.
  *
  * @example flatten({"a": {"b": {"c": "d"}}}) => {"a.b.c": "d"}
  *
  */
-function flatten(object) {
-  // TODO: Implement here
-}
+const flatten = (object, parentKey = '', result = {}) => {
+  Object.keys(object).forEach((key) => {
+    const fullKey = parentKey ? `${parentKey}.${key}` : key;
+
+    if (typeof object[key] === 'object' && !Array.isArray(object[key]) && object[key] !== null) {
+      flatten(object[key], fullKey, result);
+    } else {
+      result[fullKey] = object[key];
+    }
+  });
+
+  return result;
+};
+
+module.exports = flatten;
+
 
 /**
  * Returns a nested object. Remember that the level of nesting is not specified.

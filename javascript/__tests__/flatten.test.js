e072ca6586d10b3052d6cfd1cc1e42c6c2f7a8d8
Mohammad Reza Ghasemi <mo.ghasemi@sadad.co.ir>
2024-08-12 13:09:38 +0330

    chore: reformat code style

--- javascript/__tests__/flatten.test.js
+++ javascript/__tests__/flatten.test.js
@@ -1,51 +1,51 @@
-const flatten = require('../flatten');
+const flatten = require("../flatten");
 
-describe('flatten', () => {
-  test('flattens a simple nested object', () => {
-    const input = { a: { b: { c: 'd' } } };
-    const expectedOutput = { 'a.b.c': 'd' };
+describe("flatten", () => {
+  test("flattens a simple nested object", () => {
+    const input = { a: { b: { c: "d" } } };
+    const expectedOutput = { "a.b.c": "d" };
     expect(flatten(input)).toEqual(expectedOutput);
   });
 
-  test('flattens a more complex nested object', () => {
+  test("flattens a more complex nested object", () => {
     const input = {
       a: {
         b: {
-          c: 'd',
-          e: 'f',
+          c: "d",
+          e: "f",
         },
-        g: 'h',
+        g: "h",
       },
-      i: 'j',
+      i: "j",
     };
     const expectedOutput = {
-      'a.b.c': 'd',
-      'a.b.e': 'f',
-      'a.g': 'h',
-      'i': 'j',
+      "a.b.c": "d",
+      "a.b.e": "f",
+      "a.g": "h",
+      i: "j",
     };
     expect(flatten(input)).toEqual(expectedOutput);
   });
 
-  test('handles an object with an array', () => {
+  test("handles an object with an array", () => {
     const input = {
       a: {
         b: [1, 2, 3],
       },
     };
     const expectedOutput = {
-      'a.b': [1, 2, 3],
+      "a.b": [1, 2, 3],
     };
     expect(flatten(input)).toEqual(expectedOutput);
   });
 
-  test('handles an already flat object', () => {
-    const input = { 'a.b.c': 'd' };
-    const expectedOutput = { 'a.b.c': 'd' };
+  test("handles an already flat object", () => {
+    const input = { "a.b.c": "d" };
+    const expectedOutput = { "a.b.c": "d" };
     expect(flatten(input)).toEqual(expectedOutput);
   });
 
-  test('handles an empty object', () => {
+  test("handles an empty object", () => {
     const input = {};
     const expectedOutput = {};
     expect(flatten(input)).toEqual(expectedOutput);

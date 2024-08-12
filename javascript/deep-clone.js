080fd7d0007b9413415b341bf27c0104d756b337
Mohammad Reza Ghasemi <mo.ghasemi@sadad.co.ir>
2024-08-12 11:48:13 +0330

    chore: implement deep clone function with smaller code

--- javascript/deep-clone.js
+++ javascript/deep-clone.js
@@ -7,44 +7,27 @@
  * @returns An Literal exact copy of the arg
  */
 const deepClone = (arg) => {
-  if (arg === null || typeof arg !== 'object') {
-    return arg;
-  }
+  if (arg === null || typeof arg !== "object") return arg;
 
-  if (arg instanceof Date) {
-    return new Date(arg.getTime());
-  }
+  if (Array.isArray(arg)) return arg.map(deepClone);
 
-  if (arg instanceof RegExp) {
-    return new RegExp(arg.source, arg.flags);
-  }
+  if (arg instanceof Date) return new Date(arg);
 
-  if (Array.isArray(arg)) {
-    return arg.map(item => deepClone(item));
-  }
+  if (arg instanceof RegExp) return new RegExp(arg);
 
-  if (arg instanceof Map) {
-    return new Map(Array.from(arg.entries()).map(([key, value]) => [deepClone(key), deepClone(value)]));
-  }
+  if (arg instanceof Map) return new Map(arg);
 
-  if (arg instanceof Set) {
-    return new Set(Array.from(arg.values()).map(value => deepClone(value)));
-  }
+  if (arg instanceof Set) return new Set(arg);
 
-  if (arg instanceof Function) {
-    return arg.bind({});
-  }
+  if (arg instanceof Function) return arg.bind({});
 
-  // Handle generic objects
   const copy = {};
 
   for (const key in arg) {
-    if (arg.hasOwnProperty(key)) {
-      copy[key] = deepClone(arg[key]);
-    }
+    copy[key] = deepClone(arg[key]);
   }
 
   return copy;
-}
+};
 
 module.exports = deepClone;
